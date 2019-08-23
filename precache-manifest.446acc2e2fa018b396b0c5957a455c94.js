f71b8fbd86b9cf5255f1830c30e91ed8af5b51cf
Dylan Quesada <dquesada@Dylans-MacBook-Pro.local>
2019-08-22 21:01:51 -0500

    Updates

---
+++ precache-manifest.446acc2e2fa018b396b0c5957a455c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab8b5af8e85d8048455c8445f03722f",
+    "url": "/rick-n-morty-app/index.html"
+  },
+  {
+    "revision": "eb7af395bcbbc5b2a978",
+    "url": "/rick-n-morty-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ec6a4e2a4bf057bc8541",
+    "url": "/rick-n-morty-app/static/js/2.f7a77172.chunk.js"
+  },
+  {
+    "revision": "eb7af395bcbbc5b2a978",
+    "url": "/rick-n-morty-app/static/js/main.07b35642.chunk.js"
+  },
+  {
+    "revision": "612ec77f46853b2ac35f",
+    "url": "/rick-n-morty-app/static/js/runtime~main.6a28dfc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rick-n-morty-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
